53b7d78b59ac5071d8739d118b8549b57822c048
guicosta01 <guiferreiracosta@hotmail.com>
2024-07-21 18:46:10 -0300

    add faq page

---
+++ play-athlete-site/src/components/FaqContent/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Container, Typography, Accordion, AccordionSummary, AccordionDetails} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import styles from './FaqContent.module.css';
+
+function FaqContent() {
+    const faqs = [
+        {
+            question: "O QUE É O FAQ?",
+            answer: "FAQ significa Perguntas Frequentes. É uma lista de perguntas e respostas comuns sobre um determinado tópico."
+        },
+        {
+            question: "COMO POSSO ENTRAR EM CONTATO COM O SUPORTE?",
+            answer: "Você pode entrar em contato com o suporte através do nosso formulário de contato ou pelo e-mail suporte@exemplo.com."
+        },
+        {
+            question: "QUAL É O HORÁRIO DE ATENDIMENTO?",
+            answer: "Nosso horário de atendimento é de segunda a sexta, das 9h às 18h."
+        }
+    ];
+
+    return (
+        <Container className={styles.faq}>
+            <h2>Perguntas Frequentes</h2>
+            <hr/>
+
+            {faqs.map((faq, index) => (
+                <Accordion key={index}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <h3>{faq.question}</h3>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <h4>{faq.answer}</h4>
+                    </AccordionDetails>
+                </Accordion>
+            ))}
+        </Container>
+    );
+}
+
+export default FaqContent;
--- play-athlete-site/src/pages/Faq/index.js
+++ play-athlete-site/src/pages/Faq/index.js
@@ -1,13 +1,13 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-
+import FaqContent from "../../components/FaqContent";
 
 function Faq() {
    
     return (
         <>
             <Header/>
-                Faq
+                <FaqContent/>
             <Footer/>
         </>
     );
