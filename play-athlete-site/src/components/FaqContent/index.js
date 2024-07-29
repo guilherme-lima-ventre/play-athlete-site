4d59aac4d2f5db017fc11a57961b3ac389750460
guicosta01 <guiferreiracosta@hotmail.com>
2024-07-29 09:36:17 -0300

    faq content updated

--- play-athlete-site/src/components/FaqContent/index.js
+++ play-athlete-site/src/components/FaqContent/index.js
@@ -6,28 +6,40 @@ import styles from './FaqContent.module.css';
 function FaqContent() {
     const faqs = [
         {
-            question: "O QUE É O FAQ?",
-            answer: "FAQ significa Perguntas Frequentes. É uma lista de perguntas e respostas comuns sobre um determinado tópico."
+            question: "COMO FUNCIONA EM RELAÇÃO A ALIMENTAÇÃO E MORADIA?",
+            answer: "A maioria das universidades nos Estados Unidos oferece opções de alimentação e moradia dentro do campus para estudantes-atletas.Os custos e a disponibilidade podem variar, mas muitas bolsas de estudo cobrem total ou parcialmente essas despesas."
         },
         {
-            question: "COMO POSSO ENTRAR EM CONTATO COM O SUPORTE?",
-            answer: "Você pode entrar em contato com o suporte através do nosso formulário de contato ou pelo e-mail suporte@exemplo.com."
+            question: "QUANTO TEMPO DEMORA A GRADUAÇÃO?",
+            answer: "O tempo para completar a graduação nos Estados Unidos geralmente é de quatro anos para cursos de bacharelado. No entanto, pode variar dependendo do curso escolhido e da carga horária do estudante. Nossos consultores ajudam a planejar um cronograma acadêmico que equilibre os compromissos esportivos e acadêmicos."
         },
         {
-            question: "QUAL É O HORÁRIO DE ATENDIMENTO?",
-            answer: "Nosso horário de atendimento é de segunda a sexta, das 9h às 18h."
+            question: "QUAIS ESPORTES VOCÊS TRABALHAM?",
+            answer: "Trabalhamos com uma ampla gama de esportes, incluindo natação, futebol, basquete, atletismo, vôlei, tênis, golfe, polo aquático, entre outros. Se você pratica um esporte diferente, entre em contato conosco para verificar a possibilidade de suporte."
         },
         {
-            question: "COMO POSSO ENTRAR EM CONTATO COM O SUPORTE?",
-            answer: "Você pode entrar em contato com o suporte através do nosso formulário de contato ou pelo e-mail suporte@exemplo.com."
+            question: "EU POSSO TRABALHAR ENQUANTO ESTIVER LÁ?",
+            answer: "Sim, estudantes internacionais com visto F-1 podem trabalhar no campus até 20 horas por semana durante o período letivo e até 40 horas por semana durante as férias. Existem também opções de estágios e treinamento prático (CPT e OPT) que podem permitir trabalho fora do campus em determinadas circunstâncias."
         },
         {
-            question: "COMO POSSO ENTRAR EM CONTATO COM O SUPORTE?",
-            answer: "Você pode entrar em contato com o suporte através do nosso formulário de contato ou pelo e-mail suporte@exemplo.com."
+            question: "O QUE A BOLSA DE ESTUDOS ENGLOBA?",
+            answer: "As bolsas de estudo esportivas podem cobrir parcial ou integral as despesas com mensalidades, moradia, alimentação, livros, materiais acadêmicos e materiais esportivos. A cobertura exata varia dependendo da universidade e do programa esportivo que você se encaixa. Trabalharemos com você para identificar as melhores oportunidades disponíveis."
         },
         {
-            question: "COMO POSSO ENTRAR EM CONTATO COM O SUPORTE?",
-            answer: "Você pode entrar em contato com o suporte através do nosso formulário de contato ou pelo e-mail suporte@exemplo.com."
+            question: "COMO FUNCIONA O PROCESSO?",
+            answer: "Nosso processo começa com uma avaliação detalhada do seu perfil esportivo e acadêmico. Em seguida, ajudamos na preparação de vídeos de desempenho, cartas de recomendação e outros materiais necessários. Depois, entramos em contato com os treinadores de universidades para encontrar as melhores oportunidades de bolsas de estudo para você. Acompanhamos você em todas as etapas, desde a inscrição até a adaptação na universidade."
+        },
+        {
+            question: "QUAL IDADE POSSO INICIAR O PROGRAMA COM A PLAYATHLETE?",
+            answer: "Atletas podem começar a preparação com a PlayAthlete a partir dos 14 anos. No entanto, a maioria dos candidatos tem entre 16 e 18 anos, quando estão se preparando para terminar o ensino médio e iniciar a universidade. A vantagem de começar o trabalho mais cedo é que podemos planejar cada passo, desde desempenho escolar no ensino médio até um aumento de performance no esporte."
+        },
+        {
+            question: "AINDA NÃO SEI QUAL CURSO FAZER, ISSO PODE ATRAPALHAR?",
+            answer: "Não, isso não será um problema. Muitas universidades nos Estados Unidos permitem que os estudantes iniciem seus estudos sem declarar um curso específico, oferecendo tempo para explorar diferentes áreas antes de escolher uma especialização. Nossos consultores também podem ajudar a orientá-lo na escolha de um curso que combine com seus interesses e habilidades."
+        },
+        {
+            question: "QUAL O NÍVEL DE INGLÊS NECESSÁRIO?",
+            answer: "O nível de inglês necessário varia de acordo com a universidade, mas geralmente é exigido um bom desempenho em testes como SAT, DUOLINGUO e/ou TOEFL. Oferecemos orientação e recursos para ajudar nossos atletas a atingir o nível de inglês necessário para se candidatar às universidades. "
         },
     ];
 
