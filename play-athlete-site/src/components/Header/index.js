8bb6bca453ac2e440b75da41a43e809b64ae9fce
guilherme-lima-ventre <guilherme.ventre@gmail.com>
2024-07-30 13:47:39 -0300

    header fix

--- play-athlete-site/src/components/Header/index.js
+++ play-athlete-site/src/components/Header/index.js
@@ -1,21 +1,42 @@
 import styles from "./Header.module.css";
 import MediasNavigation from "../MediasNavigation";
 import PagesNavigation from "../PagesNavigation";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
 
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 1000);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 1000);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
         <section className={styles.header}>
+            {isSmallScreen ? 
+                null :
                 <Link to="/">
-                <img 
-                  className={styles.header}
-                  src="../../images/logo-play-athlete.svg" 
-                  alt="Logo PlayAthelte">
-                </img>
+                    <img 
+                    className={styles['header-img']}
+                    src="../../images/logo-play-athlete.svg" 
+                    alt="Logo PlayAthelte">
+                    </img>
                 </Link>
+            }   
             <PagesNavigation/>
-            <MediasNavigation/>
+            {isSmallScreen ? 
+                null :
+                <MediasNavigation className={styles.medias}/>
+            }
         </section>
     )
 }
